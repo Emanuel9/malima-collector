c80080644cf7685815fb4a26d0dfd331d1e366d5
a13xma1c <alexmcn07@gmail.com>
2019-09-02 14:12:07 +0300

    improved coverage

--- src/test/java/com/orange/malimacollector/controller/JiraControllerTest.java
+++ src/test/java/com/orange/malimacollector/controller/JiraControllerTest.java
@@ -1,12 +1,47 @@
 package com.orange.malimacollector.controller;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
+import org.springframework.web.servlet.View;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class JiraControllerTest {
+    @InjectMocks
+    JiraController controller;
+
+    @Autowired
+    MockMvc mvc;
+
+    @MockBean
+    View mockView;
+
+    @Before
+    public void setUp() throws Exception{
+        MockitoAnnotations.initMocks(this);
+        StandaloneMockMvcBuilder standaloneMockMvcBuilder = standaloneSetup(controller);
+        standaloneMockMvcBuilder.setSingleView(mockView);
+        mvc = standaloneMockMvcBuilder.build();
+    }
 
     @Test
-    void jiraDisplay() {
+    public void jiraDisplay() throws Exception {
+        mvc.perform(get("/jira"))
+                .andExpect(status().isOk());
     }
 }

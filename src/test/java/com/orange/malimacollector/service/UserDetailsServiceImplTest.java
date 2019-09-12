ecc080661f424447e3ac3767c4635dd4d50f0fc5
a13xma1c <alexmcn07@gmail.com>
2019-09-12 14:06:10 +0300

    improved code coverage

--- src/test/java/com/orange/malimacollector/service/UserDetailsServiceImplTest.java
+++ src/test/java/com/orange/malimacollector/service/UserDetailsServiceImplTest.java
@@ -1,17 +1,21 @@
 package com.orange.malimacollector.service;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertNotNull;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 class UserDetailsServiceImplTest {
-
-    @BeforeEach
-    void setUp() {
-    }
+    @Autowired
+    UserDetailsServiceImpl service;
 
     @Test
     void loadUserByUsername() {
+        assertNotNull(service.loadUserByUsername("alexm"));
     }
 }
